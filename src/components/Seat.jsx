2c090dad5c58ec23e2f2cbdba734d9ba4ddd0b51
AAshe1224 <5wzycxrykx@privaterelay.appleid.com>
2026-03-25 17:08:36 -0400

    3.25.26 Class

--- src/components/Seat.jsx
+++ src/components/Seat.jsx
@@ -1,27 +1,27 @@
- import React from "react";
+import React from "react";
 import "../../css/Seat.css";
 
 const Seat = () => {
-  const seatStatus = "Not finished";
+    const seatStatus = "Not finished";
 
-  return (
-    <>
-      <main className='room-seats'>
-        <div className="seat-1 block">
-          <p>Seat #...</p>
-          <p>Status: {seatStatus}</p>
-        </div>
-        <div className="seat-2 block">
-          <p>Seat #...</p>
-          <p>Status: {seatStatus}</p>
-        </div>
-        <div className="seat-3 block">
-          <p>Seat #...</p>
-          <p>Status: {seatStatus}</p>
-        </div>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className='room-seats'>
+                <div className="seat-1 block">
+                    <p>Seat #1</p>
+                    <p>Status: {seatStatus}</p>
+                </div>
+                <div className="seat-2 block">
+                    <p>Seat #2</p>
+                    <p>Status: {seatStatus}</p>
+                </div>
+                <div className="seat-3 block">
+                    <p>Seat #3</p>
+                    <p>Status: {seatStatus}</p>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default Seat;
