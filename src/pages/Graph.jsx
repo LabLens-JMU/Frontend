570f7d935f3f773c9514c92d99032d8623336b95
AAshe1224 <5wzycxrykx@privaterelay.appleid.com>
2026-04-20 15:04:03 -0400

    Added the Graph Code and updated installed chart.js

--- src/pages/Graph.jsx
+++ src/pages/Graph.jsx
@@ -1,11 +1,97 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import '../../css/Graph.css'; // Import your new CSS file
+
+// Register the components we need from Chart.js
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const Graph = () => {
+  const labels = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
+
+  const options = {
+    responsive: true,
+    interaction: {
+      mode: 'index',
+      intersect: false,
+    },
+    stacked: false,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Engeo Seat Availability Over 24 Hours',
+      },
+    },
+    scales: {
+      y: {
+        type: 'linear',
+        display: true,
+        position: 'left',
+      },
+      y1: {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        grid: {
+          drawOnChartArea: false,
+        },
+      },
+      y2: {
+        type: 'linear',
+        display: true,
+        position: 'right',
+      }
+    },
+  };
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Room 2020',
+        data: [2, 5, 7, 9, 2, 5, 7, 3, 4, 5, 6, 7, 7, 5, 3, 2, 8, 9, 0, 8, 6, 5, 4, 3],
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        yAxisID: 'y',
+      },
+      {
+        label: 'Room 2037',
+        data: [1, 2, 5, 6, 2, 7, 4, 5, 6, 8, 4, 5, 8, 4, 2, 0, 8, 8, 0, 0, 0, 8, 6, 4],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        yAxisID: 'y1',
+      },
+      {
+        label: 'Room 2039',
+        data: [0, 1, 3, 2, 1, 4, 2, 3, 4, 5, 3, 4, 5, 3, 1, 0, 6, 7, 0, 6, 4, 3, 2, 1],
+        borderColor: 'rgb(75, 192, 192)',
+        backgroundColor: 'rgba(75, 192, 192, 0.5)',
+        yAxisID: 'y2',
+      }
+    ],
+  };
+
   return (
-    <>
-      <div className="graph-container">
-        <h2>Graph Page</h2>
-        <p>This is where the graph will be displayed.</p>
-      </div>
-    </>
+    <div className="chart-container">
+      <Line options={options} data={data} />
+    </div>
   );
 };
 
